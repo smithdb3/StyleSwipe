58f8c7e2358fb5863e2f762a8c7d674a1000fccc
Gavin Whitaker <gavinwhit8@gmail.com>
2026-02-28 12:38:05 -0700

    demo demo

---
+++ app/_clothingImageMap.js
@@ -0,0 +1,28 @@
+/**
+ * Demo clothing images — required from app/assets so Metro resolves on web.
+ * Used by lib/clothingDemoImages.js. Underscore prefix avoids Expo Router route.
+ */
+module.exports = {
+  1: require('./assets/clothing_demo/1-grey-streetware.png'),
+  2: require('./assets/clothing_demo/2-tan-modern.png'),
+  3: require('./assets/clothing_demo/3-black-streetware.png'),
+  4: require('./assets/clothing_demo/4-tan-formal.png'),
+  5: require('./assets/clothing_demo/5-black-casual.png'),
+  6: require('./assets/clothing_demo/6-green-casual.png'),
+  7: require('./assets/clothing_demo/7grey-streetware.png'),
+  8: require('./assets/clothing_demo/8-modern.png'),
+  9: require('./assets/clothing_demo/9-grey-streetware.png'),
+  10: require('./assets/clothing_demo/10-black-casual.png'),
+  11: require('./assets/clothing_demo/11-black-casual.png'),
+  12: require('./assets/clothing_demo/12-white-casual.png'),
+  13: require('./assets/clothing_demo/13-black-formal.png'),
+  14: require('./assets/clothing_demo/14-white-.png'),
+  15: require('./assets/clothing_demo/15-modern.png'),
+  16: require('./assets/clothing_demo/16-black-casual.png'),
+  17: require('./assets/clothing_demo/17-red-streetware.png'),
+  18: require('./assets/clothing_demo/18-black-casual.png'),
+  19: require('./assets/clothing_demo/19-modern.png'),
+  20: require('./assets/clothing_demo/20-modern.png'),
+  21: require('./assets/clothing_demo/21-grey-streetware.png'),
+  22: require('./assets/clothing_demo/22-white-formal.png'),
+};
